16dc0a4c49ebfdbe96bbda764b44b6835692144f
ZUBAIR-OP <zubairlegend18@gmail.com>
2025-08-13 22:11:12 +0500

    new react Project

---
+++ Theme Switcher/src/context/theme.js
@@ -0,0 +1,16 @@
+import { useContext ,createContext } from "react";
+
+export const ThemeContext = createContext({
+    thememode:'light',
+    darktheme:() =>{},
+    lighttheme: () =>{},
+
+});
+
+
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme() {
+    return useContext(ThemeContext);
+};
