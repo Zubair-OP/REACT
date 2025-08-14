42324018944adb4c5b2ae520af76573c23101295
ZUBAIR-OP <zubairlegend18@gmail.com>
2025-08-14 12:51:04 +0500

    Todo app

---
+++ reduxtoolkit/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Adtodo from './components/Adtodo'
+import Todo from './components/Todo'
+
+function App() {
+
+  return (
+    <>
+      <h1>Learn Reduxtoolkit</h1>
+      <Adtodo />
+      <Todo />
+    </>
+  )
+}
+
+export default App
